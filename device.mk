c1d21edc7653165043f09be991115ac02841569a
Himanshu Raj <himanshurajkush@gmail.com>
2022-04-10 17:39:47 +0530

    device :  removed libandroidicu

--- device.mk
+++ device.mk
@@ -67,7 +67,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 #TARGET_RECOVERY_DEVICE_MODULES += libion libandroidicu vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
 TARGET_RECOVERY_DEVICE_MODULES += \
-    libandroidicu \
     libion \
     libxml2
 
@@ -82,8 +81,5 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
 #    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
 #    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
 
-PRODUCT_COPY_FILES += \
-   $(OUT_DIR)/target/product/phoenix/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
-#
 TW_USE_FSCRYPT_POLICY := 1
 #
